e8108085aac421f52c8335f917429e2d362e528d
MD. Humaun Kobir <118617607+hajsvdbdvhdnd@users.noreply.github.com>
2022-12-12 08:36:51 +0600

    Create Talha-dek.py

---
+++ Talha-dek.py
@@ -0,0 +1,47 @@
+আসসালামু আলাইকুম 🥀
+
+Cloning tools নিয়ে আসলাম😶🥱
+
+Subject : Public Cloning tools
+
+Tools name : b-update-
+
+পোস্টে ভালো সাপোর্ট পেলে নেক্সট cloning tools নিয়ে হাজির হবো ইনশাআল্লাহ্ 🥀🥱
+
+Method : [1]. MOBILE
+
+                 [2]. MBASIC [ best ]
+
+#0K_CP_MIX
+
+#run_tools👇
+
+pkg update && upgrade -y
+
+pkg install python
+
+pkg install git -y
+
+pip install requests
+
+pip install bs4
+
+pip install rich
+
+pip install mechanize
+
+rm -rf b-update-
+
+git clone https://github.com/DH-Alamin/🥱b-update-
+
+git clone https://github.com/DH-Alamin/🥱b-update-
+
+cd b-update-
+
+ls
+
+python run🥱.py
+
+Remove Emoji 🥱🥱
+
+Enjoy guys 🥱🥱👌
